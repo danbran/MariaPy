be8525b4d706b547f7a159b8a463faa8c884d212
danbran <danbran@posteo.de>
2020-10-08 13:32:04 +0200

    update readme and init

--- mariapy/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-try:
-    from .password import ALPHAVANTAGE_API_KEY 
-    os.environ["ALPHAVANTAGE_API_KEY"] = ALPHAVANTAGE_API_KEY
-except ImportError as error:
-    print("no alpha vantage key available", error)
-    pass
-
---
+++ readme.md
@@ -0,0 +1,7 @@
+# MariaPy
+
+MariaPy is hosted on GitHub. Clone MariaPy into another repo that is connected to a database:
+
+```shell
+git clone -b main --single-branch git@github.com:danbran/MariaPy.git
+```
